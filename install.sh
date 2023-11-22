1d1e2e6d366b1186119491b38e476624ecc9d0ce
Jefferson Hipolito <149891602+sshturbo@users.noreply.github.com>
2023-11-22 13:52:56 -0300

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -6,101 +6,90 @@ print_centered() {
     text="$1"
     padding=$(( (term_width - ${#text}) / 2 ))
     printf "%${padding}s" '' # Adiciona espaços antes do texto
-    echo -e "$text"
+    echo "$text"
 }
 
-# Verifica se o terminal suporta cores
-if tput setaf 1 &> /dev/null; then
-    # Cor azul escura
-    BLUE="\033[0;34m"
-    NC="\033[0m" # Sem cor
-else
-    # Desativa as cores se o terminal não suportar
-    BLUE=""
-    NC=""
-fi
-
-print_centered "${BLUE}Iniciando o processo de instalação...${NC}"
+print_centered "Iniciando o processo de instalação..."
 
 # Função para simular uma barra de progresso
 progress_bar() {
-    echo -n "${BLUE}Progresso: ["
+    echo -n "Progresso: ["
     for i in $(seq 1 $1); do
         echo -n "###"
         sleep 1
     done
-    echo "] Completo!${NC}"
+    echo "] Completo!"
 }
 
 # Atualizando e atualizando os pacotes
-print_centered "${BLUE}Atualizando pacotes...${NC}"
+print_centered "Atualizando pacotes..."
 sudo apt update &>/dev/null && sudo apt upgrade -y &>/dev/null
 progress_bar 5
 
 # Verificando e instalando o Node.js se necessário
 if ! command -v node > /dev/null 2>&1; then
-    print_centered "${BLUE}Node.js não está instalado. Instalando...${NC}"
+    print_centered "Node.js não está instalado. Instalando..."
     wget https://raw.githubusercontent.com/sshturbo/m-dulo/main/nodesource_setup.sh -O nodesource_setup.sh &>/dev/null
     chmod +x nodesource_setup.sh
     sudo ./nodesource_setup.sh &>/dev/null
     sudo apt-get install -y nodejs &>/dev/null
     progress_bar 10
 else
-    print_centered "${BLUE}Node.js já está instalado.${NC}"
+    print_centered "Node.js já está instalado."
 fi
 
 # Verificando e instalando o npm se necessário
 if ! command -v npm > /dev/null 2>&1; then
-    print_centered "${BLUE}npm não está instalado. Instalando...${NC}"
+    print_centered "npm não está instalado. Instalando..."
     sudo apt install npm -y &>/dev/null
     progress_bar 5
 else
-    print_centered "${BLUE}npm já está instalado.${NC}"
+    print_centered "npm já está instalado."
 fi
 
 # Verificando e instalando o dos2unix se necessário
 if ! command -v dos2unix > /dev/null 2>&1; then
-    print_centered "${BLUE}dos2unix não está instalado. Instalando...${NC}"
+    print_centered "dos2unix não está instalado. Instalando..."
     sudo apt install dos2unix -y &>/dev/null
     progress_bar 5
 else
-    print_centered "${BLUE}dos2unix já está instalado.${NC}"
+    print_centered "dos2unix já está instalado."
 fi
 
 # Verificando e instalando o PM2 se necessário
 if ! command -v pm2 > /dev/null 2>&1; then
-    print_centered "${BLUE}PM2 não está instalado. Instalando...${NC}"
+    print_centered "PM2 não está instalado. Instalando..."
     sudo npm install -g pm2 &>/dev/null
     progress_bar 5
 else
-    print_centered "${BLUE}PM2 já está instalado.${NC}"
+    print_centered "PM2 já está instalado."
 fi
 
 # Verifica se o diretório /opt/myapp/ existe
 if [ -d "/opt/myapp/" ]; then
-    print_centered "${BLUE}Diretório /opt/myapp/ já existe. Parando e excluindo processo do PM2 se existir...${NC}"
+    print_centered "Diretório /opt/myapp/ já existe. Parando e excluindo processo do PM2 se existir..."
     pm2 stop modulos-pro &>/dev/null
     pm2 delete modulos-pro &>/dev/null
-    print_centered "${BLUE}Excluindo arquivos e pastas antigos...${NC}"
+    print_centered "Excluindo arquivos e pastas antigos..."
     sudo rm -rf /opt/myapp/
 else
-    print_centered "${BLUE}Diretório /opt/myapp/ não existe. Criando...${NC}"
+    print_centered "Diretório /opt/myapp/ não existe. Criando..."
 fi
 
 # Criar o diretório para o aplicativo
 sudo mkdir -p /opt/myapp/
 
 # Baixar o ZIP do repositório ModulosPro diretamente no diretório /opt/myapp/
-print_centered "${BLUE}Baixando modulos-pro...${NC}"
+print_centered "Baixando modulos-pro..."
 sudo wget -P /opt/myapp/ https://github.com/sshturbo/m-dulo/raw/main/modulos-prov2.zip &>/dev/null
 
 # Extrair o ZIP diretamente no diretório /opt/myapp/ e remover o arquivo ZIP após a extração
-print_centered "${BLUE}Extraindo arquivos...${NC}"
+print_centered "Extraindo arquivos..."
 sudo unzip /opt/myapp/modulos-prov2.zip -d /opt/myapp/ &>/dev/null && sudo rm /opt/myapp/modulos-prov2.zip
 progress_bar 5
 
 # Dar permissão de execução para scripts .sh e converter para o formato Unix
-print_centered "${BLUE}Atualizando permissões...${NC}"
+print_centered "Atualizando permissões..."
 files=(
     "SshturboMakeAccount.sh"
     "ExcluirExpiradoApi.sh"
@@ -114,18 +103,18 @@ done
 
 # Instalar dependências e iniciar o serviço se o diretório existir
 if [ -d "/opt/myapp/" ]; then
-    print_centered "${BLUE}Instalando dependências do package.json...${NC}"
+    print_centered "Instalando dependências do package.json..."
     npm install --prefix /opt/myapp/ &>/dev/null
 
     # Iniciar o serviço
-    print_centered "${BLUE}Iniciando o serviço...${NC}"
+    print_centered "Iniciando o serviço..."
     npm start --prefix /opt/myapp/ &>/dev/null
     
     pm2 startup &>/dev/null
     pm2 save &>/dev/null
     progress_bar 10
 else
-    print_centered "${BLUE}Falha na instalação. Diretório /opt/myapp/ não encontrado.${NC}"
+    print_centered "Falha na instalação. Diretório /opt/myapp/ não encontrado."
 fi
 
-print_centered "${BLUE}Instalação concluída com sucesso!${NC}"
+print_centered "Instalação concluída com sucesso!"
