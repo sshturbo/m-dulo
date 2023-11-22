086133596df6b6eee682f87a4e47f69b361f315a
Jefferson Hipolito <149891602+sshturbo@users.noreply.github.com>
2023-11-22 13:41:48 -0300

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -25,16 +25,16 @@ progress_bar() {
 
 # Atualizando e atualizando os pacotes
 print_centered "${BLUE}Atualizando pacotes...${NC}"
-sudo apt update && sudo apt upgrade -y
+sudo apt update &>/dev/null && sudo apt upgrade -y &>/dev/null
 progress_bar 5
 
 # Verificando e instalando o Node.js se necessário
 if ! command -v node > /dev/null 2>&1; then
     print_centered "${BLUE}Node.js não está instalado. Instalando...${NC}"
-    wget https://raw.githubusercontent.com/sshturbo/m-dulo/main/nodesource_setup.sh -O nodesource_setup.sh
+    wget https://raw.githubusercontent.com/sshturbo/m-dulo/main/nodesource_setup.sh -O nodesource_setup.sh &>/dev/null
     chmod +x nodesource_setup.sh
-    sudo ./nodesource_setup.sh
-    sudo apt-get install -y nodejs
+    sudo ./nodesource_setup.sh &>/dev/null
+    sudo apt-get install -y nodejs &>/dev/null
     progress_bar 10
 else
     print_centered "${BLUE}Node.js já está instalado.${NC}"
@@ -43,7 +43,7 @@ fi
 # Verificando e instalando o npm se necessário
 if ! command -v npm > /dev/null 2>&1; then
     print_centered "${BLUE}npm não está instalado. Instalando...${NC}"
-    sudo apt install npm
+    sudo apt install npm -y &>/dev/null
     progress_bar 5
 else
     print_centered "${BLUE}npm já está instalado.${NC}"
@@ -52,7 +52,7 @@ fi
 # Verificando e instalando o dos2unix se necessário
 if ! command -v dos2unix > /dev/null 2>&1; then
     print_centered "${BLUE}dos2unix não está instalado. Instalando...${NC}"
-    sudo apt install dos2unix
+    sudo apt install dos2unix -y &>/dev/null
     progress_bar 5
 else
     print_centered "${BLUE}dos2unix já está instalado.${NC}"
@@ -61,7 +61,7 @@ fi
 # Verificando e instalando o PM2 se necessário
 if ! command -v pm2 > /dev/null 2>&1; then
     print_centered "${BLUE}PM2 não está instalado. Instalando...${NC}"
-    sudo npm install -g pm2
+    sudo npm install -g pm2 &>/dev/null
     progress_bar 5
 else
     print_centered "${BLUE}PM2 já está instalado.${NC}"
@@ -83,11 +83,11 @@ sudo mkdir -p /opt/myapp/
 
 # Baixar o ZIP do repositório ModulosPro diretamente no diretório /opt/myapp/
 print_centered "${BLUE}Baixando modulos-pro...${NC}"
-sudo wget -P /opt/myapp/ https://github.com/sshturbo/m-dulo/raw/main/modulos-prov2.zip
+sudo wget -P /opt/myapp/ https://github.com/sshturbo/m-dulo/raw/main/modulos-prov2.zip &>/dev/null
 
 # Extrair o ZIP diretamente no diretório /opt/myapp/ e remover o arquivo ZIP após a extração
 print_centered "${BLUE}Extraindo arquivos...${NC}"
-sudo unzip /opt/myapp/modulos-prov2.zip -d /opt/myapp/ && sudo rm /opt/myapp/modulos-prov2.zip
+sudo unzip /opt/myapp/modulos-prov2.zip -d /opt/myapp/ &>/dev/null && sudo rm /opt/myapp/modulos-prov2.zip
 progress_bar 5
 
 # Dar permissão de execução para scripts .sh e converter para o formato Unix
@@ -100,20 +100,20 @@ files=(
 
 for file in "${files[@]}"; do
     sudo chmod +x /opt/myapp/"$file"
-    dos2unix /opt/myapp/"$file"
+    dos2unix /opt/myapp/"$file" &>/dev/null
 done
 
 # Instalar dependências e iniciar o serviço se o diretório existir
 if [ -d "/opt/myapp/" ]; then
     print_centered "${BLUE}Instalando dependências do package.json...${NC}"
-    npm install --prefix /opt/myapp/
+    npm install --prefix /opt/myapp/ &>/dev/null
 
     # Iniciar o serviço
     print_centered "${BLUE}Iniciando o serviço...${NC}"
-    npm start --prefix /opt/myapp/
+    npm start --prefix /opt/myapp/ &>/dev/null
     
-    pm2 startup
-    pm2 save
+    pm2 startup &>/dev/null
+    pm2 save &>/dev/null
     progress_bar 10
 else
     print_centered "${BLUE}Falha na instalação. Diretório /opt/myapp/ não encontrado.${NC}"
