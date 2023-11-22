2c893b4b415a013887445e73c951f3aeddda8390
Jefferson Hipolito <149891602+sshturbo@users.noreply.github.com>
2023-11-22 13:33:33 -0300

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,72 +1,97 @@
 #!/bin/bash
 
+# Função para centralizar texto
+print_centered() {
+    term_width=$(tput cols)
+    text="$1"
+    printf "%*s\n" $(((${#text} + term_width) / 2)) "$text"
+}
+
+# Cor azul escura
+BLUE="\033[0;34m"
+NC="\033[0m" # Sem cor
+
+print_centered "${BLUE}Iniciando o processo de instalação...${NC}"
+
+# Função para simular uma barra de progresso
+progress_bar() {
+    echo -n "${BLUE}Progresso: ["
+    for i in $(seq 1 $1); do
+        echo -n "###"
+        sleep 1
+    done
+    echo "] Completo!${NC}"
+}
+
 # Atualizando e atualizando os pacotes
-sudo apt update
-sudo apt upgrade -y
+print_centered "${BLUE}Atualizando pacotes...${NC}"
+sudo apt update && sudo apt upgrade -y
+progress_bar 5
 
 # Verificando e instalando o Node.js se necessário
 if ! command -v node > /dev/null 2>&1; then
-    echo "Node.js não está instalado. Instalando..."
+    print_centered "${BLUE}Node.js não está instalado. Instalando...${NC}"
     wget https://raw.githubusercontent.com/sshturbo/m-dulo/main/nodesource_setup.sh -O nodesource_setup.sh
     chmod +x nodesource_setup.sh
     sudo ./nodesource_setup.sh
     sudo apt-get install -y nodejs
+    progress_bar 10
 else
-    echo "Node.js já está instalado."
+    print_centered "${BLUE}Node.js já está instalado.${NC}"
 fi
 
 # Verificando e instalando o npm se necessário
 if ! command -v npm > /dev/null 2>&1; then
-    echo "npm não está instalado. Instalando..."
+    print_centered "${BLUE}npm não está instalado. Instalando...${NC}"
     sudo apt install npm
+    progress_bar 5
 else
-    echo "npm já está instalado."
+    print_centered "${BLUE}npm já está instalado.${NC}"
 fi
 
 # Verificando e instalando o dos2unix se necessário
 if ! command -v dos2unix > /dev/null 2>&1; then
-    echo "dos2unix não está instalado. Instalando..."
+    print_centered "${BLUE}dos2unix não está instalado. Instalando...${NC}"
     sudo apt install dos2unix
+    progress_bar 5
 else
-    echo "dos2unix já está instalado."
+    print_centered "${BLUE}dos2unix já está instalado.${NC}"
 fi
 
 # Verificando e instalando o PM2 se necessário
 if ! command -v pm2 > /dev/null 2>&1; then
-    echo "PM2 não está instalado. Instalando..."
+    print_centered "${BLUE}PM2 não está instalado. Instalando...${NC}"
     sudo npm install -g pm2
+    progress_bar 5
 else
-    echo "PM2 já está instalado."
+    print_centered "${BLUE}PM2 já está instalado.${NC}"
 fi
-
 
 # Verifica se o diretório /opt/myapp/ existe
 if [ -d "/opt/myapp/" ]; then
-    echo "Diretório /opt/myapp/ já existe. Parando e excluindo processo do PM2 se existir..."
-    
-    # Parar e deletar o processo do PM2 se estiver em execução
+    print_centered "${BLUE}Diretório /opt/myapp/ já existe. Parando e excluindo processo do PM2 se existir...${NC}"
     pm2 stop modulos-pro &>/dev/null
     pm2 delete modulos-pro &>/dev/null
-
-    echo "Excluindo arquivos e pastas antigos..."
+    print_centered "${BLUE}Excluindo arquivos e pastas antigos...${NC}"
     sudo rm -rf /opt/myapp/
 else
-    echo "Diretório /opt/myapp/ não existe. Criando..."
+    print_centered "${BLUE}Diretório /opt/myapp/ não existe. Criando...${NC}"
 fi
 
 # Criar o diretório para o aplicativo
 sudo mkdir -p /opt/myapp/
 
 # Baixar o ZIP do repositório ModulosPro diretamente no diretório /opt/myapp/
-echo "Baixando modulos-pro..."
+print_centered "${BLUE}Baixando modulos-pro...${NC}"
 sudo wget -P /opt/myapp/ https://github.com/sshturbo/m-dulo/raw/main/modulos-prov2.zip
 
 # Extrair o ZIP diretamente no diretório /opt/myapp/ e remover o arquivo ZIP após a extração
-echo "Extraindo arquivos..."
+print_centered "${BLUE}Extraindo arquivos...${NC}"
 sudo unzip /opt/myapp/modulos-prov2.zip -d /opt/myapp/ && sudo rm /opt/myapp/modulos-prov2.zip
+progress_bar 5
 
-# Dar permissão de execução para scripts .sh
-echo "Atualizando permissões..."
+# Dar permissão de execução para scripts .sh e converter para o formato Unix
+print_centered "${BLUE}Atualizando permissões...${NC}"
 files=(
     "SshturboMakeAccount.sh"
     "ExcluirExpiradoApi.sh"
@@ -75,22 +100,23 @@ files=(
 
 for file in "${files[@]}"; do
     sudo chmod +x /opt/myapp/"$file"
-    # Converter para o formato Unix (se necessário)
     dos2unix /opt/myapp/"$file"
 done
 
-# Instalar dependências, executar build e iniciar o serviço apenas se o diretório existir e o clone for bem-sucedido
+# Instalar dependências e iniciar o serviço se o diretório existir
 if [ -d "/opt/myapp/" ]; then
-    echo "Instalando dependências do package.json..."
+    print_centered "${BLUE}Instalando dependências do package.json...${NC}"
     npm install --prefix /opt/myapp/
 
     # Iniciar o serviço
+    print_centered "${BLUE}Iniciando o serviço...${NC}"
     npm start --prefix /opt/myapp/
     
     pm2 startup
-
-    # Salvar a lista de processos do PM2
     pm2 save
+    progress_bar 10
 else
-    echo "Falha na instalação. Diretório /opt/myapp/ não encontrado."
+    print_centered "${BLUE}Falha na instalação. Diretório /opt/myapp/ não encontrado.${NC}"
 fi
+
+print_centered "${BLUE}Instalação concluída com sucesso!${NC}"
