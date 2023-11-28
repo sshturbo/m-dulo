80bf52305d4ad4d0f90b2c6ee1071e63d67fb68c
Jefferson Hipolito <149891602+sshturbo@users.noreply.github.com>
2023-11-28 19:55:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ fi
 # Verificando e instalando o PM2 se necessário
 if ! command -v pm2 > /dev/null 2>&1; then
     print_centered "PM2 não está instalado. Instalando..."
-    sudo npm install pm2 -g &>/dev/null
+    sudo npm install pm2@4.5.0 -g &>/dev/null
     progress_bar 5
 else
     print_centered "PM2 já está instalado."
